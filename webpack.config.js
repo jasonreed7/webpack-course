2c28a7654e8d1deb69f6080289b762bc957d5088
Jason Reed <jreed0719@gmail.com>
2017-04-18 18:46:47 -0400

    Work up to developing- linting javascript- connecting eslint with webpack

--- webpack.config.js
+++ webpack.config.js
@@ -3,10 +3,10 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 const PATHS = {
 	app: path.join(__dirname, 'app'),
-	build: path.join(__dirname, 'build')
+	build: path.join(__dirname, 'build'),
 };
 
-module.exports = {
+const commonConfig = {
 	entry: {
 		app: PATHS.app,
 	},
@@ -17,6 +17,45 @@ module.exports = {
 	plugins: [
 		new HtmlWebpackPlugin({
 			title: 'Webpack demo',
-		})
-	]
-}
+		}),
+	],
+};
+
+const productionConfig = () => commonConfig;
+
+const developmentConfig = () => {
+	const config = {
+		devServer: {
+			historyApiFallback: true,
+			stats: 'errors-only',
+			host: process.env.HOST,
+			port: process.env.PORT,
+		},
+		module: {
+			rules: [
+				{
+					test: /\.js$/,
+					enforce: 'pre',
+					loader: 'eslint-loader',
+					options: {
+						emitWarning: true,
+					},
+				},
+			],
+		},
+	};
+
+	return Object.assign(
+		{},
+		commonConfig,
+		config
+	);
+};
+
+module.exports = (env) => {
+	if (env === 'production') {
+		return productionConfig();
+	}
+
+	return developmentConfig();
+};
